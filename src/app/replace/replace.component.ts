e3917c657fd021dbe45cbe2b4b95986ab45a9fd6
HugeNeck <vill0352@algonquinlive.com>
2020-11-16 14:05:53 -0500

    can upload succesfully to database but not storage

--- src/app/replace/replace.component.ts
+++ src/app/replace/replace.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import { CatchModel } from '../shared/catch.model';
+// import { CatchModel } from '../shared/catch.model';
 import { WeatherDataService } from '../services/weather-data.service';
 import { ChosenFisherService } from '../services/chosen-fisher.service';
+import { FirebaseConnectionService } from '../services/firebase-connection.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-replace',
@@ -13,35 +16,41 @@ export class ReplaceComponent implements OnInit {
 
   title: string ='Add Fish to Well!';
 
-   fishType:CatchModel;
+   fishType:string;
    fishLength:string;
    fishWeight: string;
    currentFisher: string;
    currentWeather:string;
+   loggedBy: string;
+   fishImage: Blob;
 
   //works but need to reload page
 
-  constructor( private wds: WeatherDataService, private cfs : ChosenFisherService) { }
+  constructor( private angularAuth: AngularFireAuth, private fcs:FirebaseConnectionService, private wds: WeatherDataService, private cfs : ChosenFisherService) { }
 
   
-ngOnInit(): void {
-  this.wds.getWeatherData().subscribe(
-    data => {
-     if(data.cod !== 200)
-        this.currentWeather = data.cod
-          else this.currentWeather = data.weather[0].description
-    }
-  );
-  this.currentFisher = this.cfs.currentFisher;
-}
+  ngOnInit(): void {
+    this.wds.getWeatherData().subscribe(
+      data => {
+        if (data.cod !== 200)
+          this.currentWeather = data.cod
+        else this.currentWeather = data.weather[0].description
+      }
+    );
+    this.currentFisher = this.cfs.currentFisher;
+    this.angularAuth.currentUser.then(user => this.loggedBy = user.email);
 
- onSubmit(f : NgForm) {
-    alert(f.value.fishType + f.value.fishLength + f.value.fishWeight
-      + this.currentWeather)
-}
+  }
+
+  onSubmit(f: NgForm) {
+    if (!this.currentFisher) { alert('must select fisher'); return }
+    if (!f.value.fishImage) { alert('must select Image'); return; }
+    if (!f.value.fishLength) { alert('must select fishLength'); return; }
+    this.fcs.setCatch(f.value.fishType, f.value.fishLength, f.value.fishWeight, this.currentFisher, this.currentWeather, this.loggedBy, f.value.fishImage);
+  }
 
 
-  ngOnDestroy(): void{
+  ngOnDestroy(): void {
     // last function that is called
   }
 
--- src/app/services/firebase-connection.service.ts
+++ src/app/services/firebase-connection.service.ts
@@ -1,20 +1,54 @@
 import { Injectable } from '@angular/core';
 import {AngularFireDatabase} from '@angular/fire/database';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
 import { Observable } from 'rxjs';
 import { CatchModel } from '../shared/catch.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FirebaseConnectionService {
+export class FirebaseConnectionService { 
 
+  dataKey:string;
+  ref: AngularFireStorageReference;
+  task: AngularFireUploadTask;
 
-  constructor(public angularData: AngularFireDatabase) { }
+  constructor(private angularData: AngularFireDatabase, private angularStorage: AngularFireStorage) { }
 
   getCatches():Observable<CatchModel[]> {
-       
     return( this.angularData.list<CatchModel>('/users/u7FOtTJGasMlqIclUFNHuT0uCF72/').valueChanges()
     )
-    
+  }
+  
+  //Add catch to database
+  setCatch(fishType: string, fishLength: string, fishWeight: string, currentFisher: string, 
+    currentWeather: string, loggedBy: string, image: Blob) {
+    this.angularData.list('/users/u7FOtTJGasMlqIclUFNHuT0uCF72/').push({
+      fishType: fishType,
+      fishWeight: fishWeight,
+      fishLength: fishLength,
+      fisher: currentFisher,
+      loggedBy: loggedBy,
+      weatherData: currentWeather
+    }).then(key => {
+      this.dataKey = key.toString();
+      alert("submission successful")
+       
+      if (image != null) {  
+           this.uploadImage(image, this.dataKey);
+           this.angularData.list('users/u7FOtTJGasMlqIclUFNHuT0uCF72/' + this.dataKey).update(
+            "u7FOtTJGasMlqIclUFNHuT0uCF72", this.dataKey
+           )
+       }
+
+    });
+  }
+
+  uploadImage(image: Blob, name: string){
+    // const response = await fetch(uri);
+    // const blob = await response.blob();
+    this.ref = this.angularStorage.ref("images/" + name);
+    this.task = this.ref.put(image);
+    console.log("image upload succesful")
   }
 }
